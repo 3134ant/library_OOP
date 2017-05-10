2271379c05926f5935b29decc79020885aa4801a
Htinel <3134ant@gmail.com>
2017-05-10 16:55:30 +0300

    Update load_library.rb

--- load_library.rb
+++ load_library.rb
@@ -68,6 +68,3 @@ end
 
 index = Controllers.new
 index.test_data_load
-puts "Who often takes the book \r\n #{index.who_often_takes_the_books}"
-puts "What is the most popular book: \r\n #{index.what_is_the_most_popular_book}"
-puts "How many people ordered one of the three most popular books \r\n #{index.how_many_people_ordered_one_of_the_three_most_popular_books}"
