a52c63abdd5b0162281ec0cc3caea8967ce89748
Htinel <3134ant@gmail.com>
2017-05-10 16:51:39 +0300

    Update index.rb

--- index.rb
+++ index.rb
@@ -35,8 +35,3 @@ class Controllers
     File.write("./data.yaml", @lib.to_yaml)
   end
 end
-
-index = Controllers.new
-puts "Who often takes the book \r\n #{index.who_often_takes_the_books}"
-puts "What is the most popular book: \r\n #{index.what_is_the_most_popular_book}"
-puts "How many people ordered one of the three most popular books \r\n #{index.how_many_people_ordered_one_of_the_three_most_popular_books}"
