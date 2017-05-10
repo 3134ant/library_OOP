0d01ed2e946f6d258cc57fa1601b7ecaba6933c4
Htinel <3134ant@gmail.com>
2017-05-10 17:13:50 +0300

    Update library.rb

--- models/library.rb
+++ models/library.rb
@@ -95,11 +95,6 @@ class Library
   end
 end
 
-  
-index = Controllers.new
-index.test_data_load
-
-index = Controllers.new
 puts "Who often takes the book \r\n #{index.who_often_takes_the_books}"
 puts "What is the most popular book: \r\n #{index.what_is_the_most_popular_book}"
 puts "How many people ordered one of the three most popular books \r\n #{index.how_many_people_ordered_one_of_the_three_most_popular_books}"
